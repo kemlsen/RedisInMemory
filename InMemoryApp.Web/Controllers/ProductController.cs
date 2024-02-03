47c9dc2c956a2a9f1fe67bee27d6f29b3eddbe6b
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2024-02-03 21:52:42 +0300

    Added InMemoryCache Implementation

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using System;
 
 namespace InMemoryApp.Web.Controllers
 {
@@ -13,6 +15,43 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            options.Priority = CacheItemPriority.Low;
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}=>{value}=>sebep:{reason}");
+            });
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 200 };
+
+            _memoryCache.Set<Product>("product:1", product);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue("zaman", out string zamanCache);
+
+            _memoryCache.TryGetValue("callback", out string callback);
+
+            _memoryCache.TryGetValue("product:1", out string product);
+
+            ViewBag.zaman = zamanCache;
+
+            ViewBag.callback = callback;
+
+            ViewBag.product = product;
+
             return View();
         }
     }
