f70ca0d4f7bcf03edfe6f62ffd4c1e2f4fa76c42
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2024-02-05 01:08:12 +0300

    Added IDistributedCache Configuration

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 150 };
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            await _distributedCache.SetStringAsync("product:1", jsonProduct, options);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            string jsonProduct = _distributedCache.GetString("product:1");
+            Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.Product = product;
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            _distributedCache.Remove("name");
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            byte[] resimByte = _distributedCache.Get("resim");
+
+            return File(resimByte, "image/png");
+        }
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/download.png");
+
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("resim", imageByte);
+
+            return View();
+        }
+    }
+}
